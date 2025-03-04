f13699be91d556e9a298e0e457b89eeddfcb73de
asishsahu946 <asishsahu946@gmail.com>
2025-03-04 22:51:15 +0530

    update

--- routes/freelanceUsers.js
+++ routes/freelanceUsers.js
@@ -10,11 +10,27 @@ const router = express.Router();
 await client.connect();
 
 const db = client.db("freelanceWebsite");
-const collection = db.collection("freelanceUsers");
+const collection = db.collection("users");
 
-router.post("/freelance-users", async (req, res) => {
+router.post("/create-user", async (req, res) => {
   await collection.insertOne(req.body);
+  res.status(200).json({ message: "User created successfully" });
   console.log(req.body);
 });
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await collection.findOne({ email, password });
+  if (user) {
+    res.status(200).json({ message: "Login successful", user });
+  } else {
+    res.status(200).json({ message: "Invalid email or password" });
+  }
+});
+
+router.get("/get-user/:id", async (req, res) => {
+  const users = await collection.find({ _id: new ObjectId(req.params.id) }).toArray();
+  res.status(200).json(users);
+})
 
 export default router;
